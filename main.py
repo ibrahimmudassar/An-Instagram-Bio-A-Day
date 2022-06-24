8d963d4a8d263fd930a519dea073cdf4d86369fc
Ibrahim Mudassar <ibrahimmudassar@gmail.com>
2022-06-24 15:13:55 -0400

    added footer to bio
    i need to make sure the bio is 150 or less so i subtract the footer length and then make sure the quote + author is less than that amount

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ from instagrapi import Client
 env = Env()
 env.read_env()  # read .env file, if it exists
 
+footer = "\nMy biography changes everyday!"
+
 # max length is adjustable to your desire
 # https://github.com/lukePeavey/quotable for documentation
-response = requests.get(
-    "https://api.quotable.io/random?maxLength=80?tags=famous-quotes").json()
-full_quote = response['content'] + " - " + response['author']
+quote_length = 151  # i need to make sure the bio is 150 or less so i subtract the footer length and then make sure the quote + author is less than that amount
+while quote_length > 151 - len(footer):
+    response = requests.get(
+        "https://api.quotable.io/random?maxLength=80?tags=famous-quotes").json()
+    full_quote = response['content'] + " - " + response['author']
+    quote_length = len(full_quote)
 
 # sign in to instagram
 cl = Client()
 cl.login(env("INSTA_USERNAME"), env("INSTA_PASSWORD"))
 
+full_text = full_quote + footer
+
 print(cl.account_info().dict())
-cl.account_edit(biography=full_quote)
+cl.account_edit(biography=full_text, external_url="https://t.ly/agks")
 print(cl.account_info().dict())
+print(len(footer))
+print(len(full_text))
